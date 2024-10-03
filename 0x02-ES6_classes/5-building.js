1dfe51ba42d4fa5914fded4218d873b5602d783a
Amr Elstohy <amramr2901@gmail.com>
2024-10-04 01:48:44 +0300

    hi

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('Sqft must be a number');
-      }
-  
-      this._sqft = sqft;
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
