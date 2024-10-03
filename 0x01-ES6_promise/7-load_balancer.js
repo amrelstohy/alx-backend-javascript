fb7636952e40253ca99b629384e7d7c887d62ffe
Amr Elstohy <amramr2901@gmail.com>
2024-10-03 17:38:55 +0300

    hi

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload]).then((r => r));
+  return Promise.race([chinaDownload, USDownload]);
 }
