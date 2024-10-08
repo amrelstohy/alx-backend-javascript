8bfcd0698522eea2d76010a0a6c878424227f98f
Amr Elstohy <amramr2901@gmail.com>
2024-10-08 22:06:11 +0300

    hi

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
     const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
-    if (studentGrade.length === 0) {
-      return {
-        ...student,
-        grade: 'N/A',
-      };
-    } return {
+    const grade = studentGrade.length === 0 ? 'N/A' : studentGrade[0].grade;
+    return {
       ...student,
-      grade: studentGrade[0].grade,
+      grade,
     };
   });
 }
